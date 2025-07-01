f3b8339460ce3bf3311652aaed8d8c7c7de231a8
Royal Ibeadogbulem <royalai004@gmail.com>
2025-07-01 20:39:31 +0000

    worked on main algorithm

--- gen_api.py
+++ gen_api.py
@@ -3,7 +3,7 @@ from google import genai
 from google.genai import types
 
 # Set environment variables
-def get_response(word, synonyms):
+def get_response(content):
     my_api_key = os.getenv('GENAI_KEY')
     genai.api_key = my_api_key
     
@@ -14,8 +14,13 @@ def get_response(word, synonyms):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-        system_instruction="You are an English teacher that can find the best synonym to use in a situation and can provide examples of how to use it."
-        ),
-        contents="What are the advantages of pair programming?",
+        system_instruction="""
+        With the knowledge of an English professor, you'll be referred to as the 
+        helper for a program that specializes in finding the best synonym to use 
+        in a situation and giving examples of how to use it in a sentence.
+        The slogan of the program is "What's on the Tip of Your Tongue?" 
+        """),
+        contents=content,
     )
+
     return(response.text)
--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-import requests
+"""
+from wordapi.py import _
+
+from db.py import _
+
+"""
+from gen_api import get_response
+
+def get_user_words():
+    SENTINEL = "toyota"
+    print(f"Enter words one by one. Type '{SENTINEL} when you are finished.")
+    
+    words = []
+    while True:
+        word = input("Enter a word: ").strip()
+
+        if word.lower() == SENTINEL:
+            break
+        elif word.isalpha():
+            words.append(word.lower())
+        else:
+            print("Please try to only use letters")
+    
+    return words
+
+def get_valid_sentence():
+    
+    while True:
+        sentence = input("Enter a valid sentence: ").strip()
+        if len(sentence) > 1 and sentence[-1] in ".?!":
+            return sentence
+        else: 
+            print("Invalid sentence sentence.")
+    
+
+def main():
+    print(get_response("Briefly introduce yourself to the user."))
+    words = get_user_words()
+    sentence = get_valid_sentence()
+    synonym_dict = {}
+    for word in words:
+        synonym_dict[word] = get_synonyms(word)
+    
+
+main()
