3ffc073b6d4efed3c1626bde8064b6265b9901b8
Soliat Adeboye <adeboyesolihah2@gmail.com>
2025-07-03 15:07:13 +0000

    fixed error in gen_api.py

--- gen_api.py
+++ gen_api.py
@@ -15,7 +15,7 @@ def get_response(content):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         config=types.GenerateContentConfig(
-            "Complete the sentence only and list "
+            system_instruction="Complete the sentence only and list "
             "the top 5 synonyms from the options that can complete "
             "the sentence"
             ), contents=content,)
